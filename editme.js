42b523b6d8da3582cd0b334139bfb36e7142b252
Geno Poarch <genokcmo@gmail.com>
2015-04-22 21:05:42 -0500

    Assignment 10
    Weldon “Geno” Poarch

--- editme.js
+++ editme.js
@@ -5,7 +5,24 @@ Google Static Maps API: https://maps.googleapis.com/maps/api/staticmap?size=640x
 
 var app = angular.module("MapApp", []);
 
-app.controller("MapController", function () {
+app.controller("MapController", function ($scope, $interval, $http) {
 
-  
+	$http.jsonp('http://api.open-notify.org/iss-now.json?callback=JSON_CALLBACK').
+		success(function (data) {
+			$scope.map = 'https://maps.googleapis.com/maps/api/staticmap?size=640x480&zoom=3&markers=' + data.iss_position.latitude + ',' + data.iss_position.longitude;
+		}).
+		error(function(){
+			console.log('error');
+		});
+
+	$interval(function(){
+
+		$http.jsonp('http://api.open-notify.org/iss-now.json?callback=JSON_CALLBACK').
+			success(function (data) {
+				$scope.map = 'https://maps.googleapis.com/maps/api/staticmap?size=640x480&zoom=3&markers=' + data.iss_position.latitude + ',' + data.iss_position.longitude;
+			}).
+			error(function(){
+				console.log('error');
+			});
+	}, 10000);
 });
